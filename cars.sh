380c3d1f4243d93a2959a25fda192f99def3dc8e
Connor Lydon <connoralydon@gmail.com>
2020-12-07 15:54:24 -0800

    updated formatting

--- cars.sh
+++ cars.sh
@@ -7,8 +7,11 @@ do
 	echo "[2] Type the number 2 to display the list of cars."
 	echo "[3] Type the number 3 to quit and exit the program."
 	echo "Please type the number of the option you wish to select: "
+
 	read -r num
+
 	 case "$num" in
+
 		 "1")
 			 echo "Enter the car year: "
 			 read -r year
@@ -19,11 +22,15 @@ do
 			 NEW="${year}:${make}:${model}"
 			 echo  "$NEW" >> my_old_cars.txt
 			 ;;
+
 		 "2")
 	 		 sort  My_old_cars.txt;;
+
 		 "3")
 			 echo "Exiting, thank you for using the program";;
+
 	 	 *)
 	   		 echo "Please type 1, 2, or 3";;
+         
 	 esac
 done
